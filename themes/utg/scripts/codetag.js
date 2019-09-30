90441e3f54b1bbbba9bed5182145cee7946dfcec
Philip H <philip.habib6347@gmail.com>
2019-09-29 22:13:32 -0400

    Added syntax hihighting for C# languages with PrismJS

--- themes/utg/scripts/codetag.js
+++ themes/utg/scripts/codetag.js
@@ -1,8 +1,14 @@
 hexo.extend.tag.register('code', (args, content) => {
 
   let caption = args.join(' ');
+  let lang = "";
+  if (caption.indexOf(".cs") !== -1) {
+    lang = "csharp"
+  } else {
+    lang = "python"
+  }
   return `<figure>
   <figcaption>${caption}</figcaption>
-    <pre><code class="language-python" contenteditable spellcheck="false">${content}</code></pre>
+    <pre><code class="language-${lang}" contenteditable spellcheck="false">${content}</code></pre>
 </figure>`
 }, {ends: true})
